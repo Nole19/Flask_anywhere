01b955eb2d11dfdb25177e4fbcb6bd6a08f3f9ed
Nole19 <81470173+Nole19@users.noreply.github.com>
2024-04-08 22:29:53 +0200

    Update app.py
    Te

--- app.py
+++ app.py
@@ -5,25 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask! I hope that deployement goes well!!!!'
+    return 'Hello from Flask!'
 
 @app.route('/github-deploy', methods=['POST'])
 def github_deploy():
-    app.logger.debug('GitHub deploy endpoint hit with POST')
-    if request.method == 'POST':
-        # Assuming your project directory is correctly set up in your PythonAnywhere account
-        # and that your PythonAnywhere account has been configured to use SSH keys for GitHub
-        try:
-            # Pull the latest changes from the GitHub repository
-            # Replace '/home/yourusername/yourrepository' with the actual path to your repository on PythonAnywhere
-            subprocess.run(['git', '-C', '/home/nole19/Flask_anywhere', 'pull'], check=True)
-            # Return a success message
-            return 'Updated code successfully', 200
-        except subprocess.CalledProcessError as e:
-            # Log the error for debugging
-            print("Error occurred while pulling changes:", e)
-            return 'Failed to update code', 500
+    return 'POST request to /github-deploy received', 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=5002)
-
